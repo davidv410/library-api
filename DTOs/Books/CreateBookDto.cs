a5847843cfd34d96661072dfbfdc21c642a81d9b
davidv410 <davidvrgoc99@gmail.com>
2026-08-19 19:00:40 +0200

    CreateBookDto validation

--- DTOs/Books/CreateBookDto.cs
+++ DTOs/Books/CreateBookDto.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LibraryApi.DTOs.Books;
 
 public class CreateBookDto
 {
+    [Required]
     public string Title { get; set; } = string.Empty;
+
+    [Required]
     public string Author { get; set; } = string.Empty;
+
+    [Range(0, 2026)]
     public int ReleaseYear { get; set; }
 }
