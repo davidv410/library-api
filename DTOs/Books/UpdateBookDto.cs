010aab5d1ab391ea1e664f014c6dcd1b7690e691
davidv410 <davidvrgoc99@gmail.com>
2026-08-19 19:18:31 +0200

    UpdateBookDto validation

--- DTOs/Books/UpdateBookDto.cs
+++ DTOs/Books/UpdateBookDto.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LibraryApi.DTOs.Books;
 
 public class UpdateBookDto
 {
+    [MinLength(1)]
     public string? Title { get; set; }
+
+    [MinLength(1)]
     public string? Author { get; set; }
+
+    [Range(0, 2026)]
     public int? ReleaseYear { get; set; }
 }
