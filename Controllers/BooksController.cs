25d89ecd5483f5dc7231664fa35963c55f0763a3
davidv410 <davidvrgoc99@gmail.com>
2026-08-19 03:49:25 +0200

    post book route

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,6 +1,8 @@
 using LibraryApi.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using LibraryApi.DTOs.Books;
+using LibraryApi.Models;
 
 namespace LibraryApi.Controllers;
 
@@ -21,5 +23,22 @@ public class BooksController : ControllerBase
         var books = await _db.Books.ToListAsync();
 
         return Ok(books);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateBook(CreateBookDto dto)
+    {
+        var book = new Book
+        {
+            Title = dto.Title,
+            Author = dto.Author,
+            ReleaseYear = dto.ReleaseYear
+        };
+
+        _db.Books.Add(book);
+
+        await _db.SaveChangesAsync();
+
+        return Ok(book);
     }
 }
---
+++ DTOs/Books/CreateBookDto.cs
@@ -0,0 +1,8 @@
+namespace LibraryApi.DTOs.Books;
+
+public class CreateBookDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public int ReleaseYear { get; set; }
+}
