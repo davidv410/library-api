c61ce8f8912c31e952622f023c17b11d1448a68f
davidv410 <davidvrgoc99@gmail.com>
2026-08-21 05:01:53 +0200

    token generation

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,3 +1,7 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 using LibraryApi.Data;
 using LibraryApi.DTOs.Auth;
 using LibraryApi.Models;
@@ -9,10 +13,14 @@ namespace LibraryApi.Services;
 public class AuthService : IAuthService
 {
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly IConfiguration _configuration;
 
-    public AuthService(UserManager<ApplicationUser> userManager)
+    public AuthService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration configuration)
     {
         _userManager = userManager;
+        _signInManager = signInManager;
+        _configuration = configuration;
     }
 
     public async Task<IdentityResult> RegisterUser(RegisterUserDto dto)
@@ -24,5 +32,51 @@ public class AuthService : IAuthService
         };
 
         return await _userManager.CreateAsync(user, dto.Password);
+    }
+
+    public async Task<string?> LoginUser(LoginUserDto dto)
+    {
+        var user = await _userManager.FindByNameAsync(dto.Username);
+
+        if(user == null)
+        {
+            return null;
+        }
+
+        var result = await _signInManager.CheckPasswordSignInAsync(
+            user,
+            dto.Password,
+            false
+        );
+
+        if(!result.Succeeded)
+        {
+            return null;
+        }
+
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id),
+            new Claim(ClaimTypes.Name, user.UserName!)
+        };
+
+        var key = _configuration["Jwt:Key"]!;
+
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+
+        var credentials = new SigningCredentials(
+            securityKey,
+            SecurityAlgorithms.HmacSha256
+        );
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(1),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
