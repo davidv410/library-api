3f804d9eb7f7ca20b450043170f13d897ca1d932
davidv410 <davidvrgoc99@gmail.com>
2026-08-28 02:16:32 +0200

    BookReview model relations

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks.Dataflow;
 using LibraryApi.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Routing.Template;
@@ -14,6 +15,7 @@ public class AppDbContext : IdentityDbContext<ApplicationUser>
 
     public DbSet<Book> Books => Set<Book>();
     public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+    public DbSet<BookReview> BookReviews => Set<BookReview>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -24,5 +26,21 @@ public class AppDbContext : IdentityDbContext<ApplicationUser>
             .WithMany(u => u.RefreshTokens)
             .HasForeignKey(rt => rt.UserId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<BookReview>()
+            .HasOne(br => br.Book)
+            .WithMany()
+            .HasForeignKey(br => br.BookId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<BookReview>()
+            .HasOne(br => br.User)
+            .WithMany()
+            .HasForeignKey(br => br.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<BookReview>()
+            .HasIndex(br => new { br.BookId, br.UserId })
+            .IsUnique();
     }
 }
