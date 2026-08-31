61054101bc080b94c3f6364651465d2910407774
davidv410 <davidvrgoc99@gmail.com>
2026-08-31 03:50:05 +0200

    CreateBookReview nullable

--- Services/IBookReviewService.cs
+++ Services/IBookReviewService.cs
@@ -3,5 +3,5 @@ using LibraryApi.DTOs.BookReviews;
 namespace LibraryApi.Services;
 public interface IBookReviewService
 {
-    Task<BookReviewResponseDto> CreateBookReview (int bookId, string userId, CreateBookReviewDto dto);
+    Task<BookReviewResponseDto?> CreateBookReview (int bookId, string userId, CreateBookReviewDto dto);
 }
