7c24545599c62ab20672f3bda3eb5ed1d79dabff
davidv410 <davidvrgoc99@gmail.com>
2026-08-31 17:56:00 +0200

    custom errorHandler

---
+++ Exceptions/AppException.cs
@@ -0,0 +1,11 @@
+namespace LibraryApi.Exceptions;
+
+public class AppException : Exception
+{
+    public int StatusCode { get; }
+
+    public AppException(int statusCode, string message) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}
