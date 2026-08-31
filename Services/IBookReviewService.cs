764b986dc4cc887a1857ef1442bb05df6808a798
davidv410 <davidvrgoc99@gmail.com>
2026-08-31 04:25:45 +0200

    IBookReview GetBookReviews

--- Services/IBookReviewService.cs
+++ Services/IBookReviewService.cs
@@ -3,5 +3,6 @@ using LibraryApi.DTOs.BookReviews;
 namespace LibraryApi.Services;
 public interface IBookReviewService
 {
+    Task<IEnumerable<BookReviewResponseDto>?> GetBookReviews(int bookId);
     Task<BookReviewResponseDto?> CreateBookReview (int bookId, string userId, CreateBookReviewDto dto);
 }
