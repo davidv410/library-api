3e58c248b0f1da28c8a58bd51428adfce8dd1240
davidv410 <davidvrgoc99@gmail.com>
2026-08-31 03:51:02 +0200

    CreateBookReview service

--- Services/BookReviewService.cs
+++ Services/BookReviewService.cs
@@ -1,5 +1,6 @@
 using LibraryApi.Data;
 using LibraryApi.DTOs.BookReviews;
+using LibraryApi.Models;
 
 namespace LibraryApi.Services;
 
@@ -12,7 +13,40 @@ public class BookReviewService : IBookReviewService
         _db = db;
     }
 
-    public Task<BookReviewResponseDto> CreateBookReview(int bookId, string userId, CreateBookReviewDto dto)
+    public async Task<BookReviewResponseDto?> CreateBookReview(int bookId, string userId, CreateBookReviewDto dto)
     {
+        if(string.IsNullOrWhiteSpace(userId))
+        {
+            return null;
+        }
+
+        var bookExists = await _db.Books.FindAsync(bookId);
+        if(bookExists == null)
+        {
+            return null;
+        }
+
+        var bookReview = new BookReview
+        {
+            Review = dto.Review,
+            Rating = dto.Rating,
+            BookId = bookId,
+            UserId = userId
+        };
+
+        _db.BookReviews.Add(bookReview);
+
+        await _db.SaveChangesAsync();
+
+        return new BookReviewResponseDto
+        {
+            Id = bookReview.Id,
+            Review = bookReview.Review,
+            Rating = bookReview.Rating,
+            LikeCount = bookReview.LikeCount,
+            DislikeCount = bookReview.DislikeCount,
+            BookId = bookReview.BookId,
+            UserId = bookReview.UserId
+        };
     }
 }
