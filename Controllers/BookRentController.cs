b69daa4f4f2ed7e253d09972924ad2ace06a0210
davidv410 <davidvrgoc99@gmail.com>
2026-09-04 02:59:33 +0200

    RentBook

--- Controllers/BookRentController.cs
+++ Controllers/BookRentController.cs
@@ -1,3 +1,6 @@
+using System.Security.Claims;
+using LibraryApi.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryApi.Controllers;
@@ -6,4 +9,18 @@ namespace LibraryApi.Controllers;
 [Route("api/books/{bookId}/rent")]
 public class BookRentController : ControllerBase
 {
+    private readonly IBookRentService _bookRentService;
+
+    public BookRentController(IBookRentService bookRentService)
+    {
+        _bookRentService = bookRentService;
+    }
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> RentBook (int bookId)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+        var rent = _bookRentService.RentBook(bookId, userId);
+        return Ok(rent);
+    }
 }
